391c9e9e2a5ddc98f5fce96699a3eebb9f8bdc75
Joacim-S <45919018+Joacim-S@users.noreply.github.com>
2024-08-11 17:31:03 +0300

    7b tehty

---
+++ osa7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,48 @@
+import { useField } from '../hooks'
+
+const CreateNew = (props) => {
+  const [content, resetContent] = useField('text')
+  const [author, resetAuthor] = useField('text')
+  const [info, resetInfo] = useField('text')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0
+    })
+  }
+
+  const resetFields = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit} onReset={resetFields}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default CreateNew
